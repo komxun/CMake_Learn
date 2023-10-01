a1f1f9ede1a15e0c4ced2ab8ae820c4941f4347f
komxun <komxun@gmail.com>
2023-10-01 16:25:05 +0700

    new update

---
+++ try_library/build/CMakeFiles/trim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trim.dir/main.cpp.o"
+  "trim"
+  "trim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
